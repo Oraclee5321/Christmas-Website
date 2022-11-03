2a4592b88c8048fefaa62d2fdd5006831190695d
Oraclee5321 <98158236+Oraclee5321@users.noreply.github.com>
2022-11-03 10:20:55 +0000

    Added all the card images

---
+++ game-script.js
@@ -0,0 +1,8 @@
+$("#card_back_1").click(function(){
+    $("#card_back_1").hide();
+    $("#card_front_1").show();
+  });
+$("#card_front_1").click(function(){
+    $("#card_back_1").show();
+    $("#card_front_1").hide();
+  });
